07d7b379a104fb3cd2f4bf2e2f1fe2d92cae927a
pu0238 <michal.lustyk>
2022-08-14 20:01:05 +0200

    added test files

---
+++ tests/o.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { O } from "../target/types/o";
+
+describe("o", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.O as Program<O>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
